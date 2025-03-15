fc9f0d4756863dda2aeafd723d76e6881c222a24
Brandon_Kevin <brandonykevin07@gmail.com>
2025-03-15 08:38:26 -0400

    Testing_of_enviroment

---
+++ Clases/Prueba_Antes.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string nombre;
+    cout<<"Introdusca su nombre: ";
+    cin>>nombre;
+    cout<<"El nombre introducido seria: "<<nombre<<endl;
+    return 0;
+}
